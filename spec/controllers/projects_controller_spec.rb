78481d92c166af6ff7cd070570be9d21a2f07336
Travis Davis <tdavis@axiapamyents.com>
2014-03-26 16:54:17 -0700

    Redirect the users back to the projects page if they try going to a project htat doesn't exist.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe ProjectsController do
+  it "displays an error for a missing project" do
+    message = "The project you were looking for could not be found."
 
+    get :show, id: "not-here"
+    expect(response).to redirect_to(projects_path)
+
+    expect(flash[:alert]).to eql(message)
+  end
 end
