5190a6928768d85e473bd4e2e8b9307d9ba061f5
Travis Davis <tdavis@axiapamyents.com>
2014-03-28 16:12:54 -0700

    make projects only visible to users with permission to see them

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,22 +1,20 @@
 require 'spec_helper'
 
 describe ProjectsController do
-  it "displays an error for a missing project" do
-    message = "The project you were looking for could not be found."
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    sign_in(user)
+  end
 
+  it "displays an error for a missing project" do
     get :show, id: "not-here"
     expect(response).to redirect_to(projects_path)
+    message = "The project you were looking for could not be found."
 
     expect(flash[:alert]).to eql(message)
   end
 
-  let(:user) { FactoryGirl.create(:user) }
-
   context "standard users" do
-    before do
-      sign_in(user)
-    end
-
     { new: :get,
       create: :post,
       edit: :get,
@@ -24,10 +22,20 @@ describe ProjectsController do
       destroy: :delete }.each do |action, method|
       it "cannot access the #{action} action" do
         sign_in(user)
+
         send(method, action, :id => FactoryGirl.create(:project))
+
         expect(response).to redirect_to(root_path)
         expect(flash[:alert]).to eql("You must be an admin to do that.")
       end
+    end
+
+    it "cannot access the show action without permission" do
+      project = FactoryGirl.create(:project)
+      get :show, id: project.id
+
+      expect(response).to redirect_to(projects_path)
+      expect(flash[:alert]).to eql("The project you were looking for could not be found.")
     end
   end
 end
--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -21,3 +21,15 @@ end
 RSpec.configure do |c|
   c.include AuthHelpers, type: :controller
 end
+
+module AuthorizationHelpers
+  def define_permission!(user, action, thing)
+    Permission.create!(user: user,
+                       action: action,
+                       thing: thing)
+  end
+end
+
+RSpec.configure do |c|
+  c.include AuthorizationHelpers
+end
