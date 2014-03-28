80a14906df35931815dd01ba9e3c6222533a42fd
Travis Davis <travis.davis@gmail.com>
2014-03-27 20:42:41 -0700

    Added email to the allowed user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name,
+                                   :email,
                                    :password,
                                    :password_confirmation)
     end
